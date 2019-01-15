36a2cafe013dc648d63eaa296b064e9cec20bec1
abdullahGhoneim <abdullahghoneim890@gmail.com>
2019-01-15 20:51:13 +0200

    todo app with vuejs and vuex v1

---
+++ src/components/status.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3></h3>
+    <p>ther are currently {{linkesLength}} Todos</p>
+    <button @click="removeAllLinks">Removeall</button>
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "status",
+  computed: {
+    ...mapGetters(["linkesLength"])
+  },
+  methods: {
+    ...mapActions(["removeAll"]),
+    removeAllLinks() {
+      this.removeAll();
+    }
+  }
+};
+</script>
+
+<style>
+button {
+  text-transform: uppercase;
+  font-size: 0.8em;
+  background: #f9d0e3;
+  border: none;
+  padding: 5px;
+  /* color: #ff0076; */
+  cursor: pointer;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,39 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    title: "Todo App With Vuex",
+    links: [
+      "Learn Vue.js",
+      "learn Vuex State Management",
+      "watch forrest gump Movie"
+    ]
+  },
+  getters: {
+    linkesLength: state => {
+      return state.links.length;
+    }
   },
   mutations: {
-
+    ADD_LINK: (state, link) => {
+      return state.links.push(link);
+    },
+    REMOVE_LINK: (state, link) => {
+      return state.links.splice(link, 1);
+    },
+    REMOVE_ALL: state => {
+      return (state.links = []);
+    }
   },
   actions: {
-
+    removeLink: (context, link) => {
+      context.commit("REMOVE_LINK", link);
+    },
+    removeAll: context => {
+      context.commit("REMOVE_ALL");
+    }
   }
-})
+});
